1cc89d5c9eefe0a676c9532a2bae6387b86c48f8
rafael.s <rafael.santos@betha.com.br>
2022-06-07 11:40:57 -0300

    creating project and importing dependencies

---
+++ src/main/java/com/github/rafaelcardosodev/VendasApplication.java
@@ -0,0 +1,20 @@
+package com.github.rafaelcardosodev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class VendasApplication {
+
+    @GetMapping("/hello")
+    public String helloWord() {
+        return "hello world";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(VendasApplication.class, args);
+    }
+}
