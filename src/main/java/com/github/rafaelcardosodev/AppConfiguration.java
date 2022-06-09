015cbc1ccd051ca2b6a17db0398c63fc7d6ad404
rafael.s <rafael.santos@betha.com.br>
2022-06-09 16:32:19 -0300

    application.properties

--- src/main/java/com/github/rafaelcardosodev/AppConfiguration.java
+++ src/main/java/com/github/rafaelcardosodev/AppConfiguration.java
@@ -6,8 +6,4 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfiguration {
 
-    @Bean(name = "applicationName")
-    public String applicationName() {
-        return "Sistema de Vendas";
-    }
 }
--- src/main/java/com/github/rafaelcardosodev/VendasApplication.java
+++ src/main/java/com/github/rafaelcardosodev/VendasApplication.java
@@ -2,9 +2,9 @@ package com.github.rafaelcardosodev;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VendasApplication {
 
-    @Autowired
-    @Qualifier("applicationName")
+    @Value("${application.name}")
     private String applicationName;
 
     @GetMapping("/hello")
