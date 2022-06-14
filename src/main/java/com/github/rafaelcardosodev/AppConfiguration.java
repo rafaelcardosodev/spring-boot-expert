8eab39f7f4ad5b12666143de9df2277c731377ca
rafael.s <rafael.santos@betha.com.br>
2022-06-14 15:03:56 -0300

    creating multiple profiles on .properties and configurations

--- src/main/java/com/github/rafaelcardosodev/AppConfiguration.java
+++ src/main/java/com/github/rafaelcardosodev/AppConfiguration.java
@@ -1,9 +1,17 @@
 package com.github.rafaelcardosodev;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development")
 public class AppConfiguration {
-
+    @Bean
+    public CommandLineRunner execute() {
+        return args -> {
+            System.out.println("Executing development configuration");
+        };
+    }
 }
